c59d19fa58ab8e1e31c6f619f79f65e22cba917f
IT24102178 <IT24102178@my.sliit.lk>
2025-09-16 22:24:59 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24102178.R
@@ -0,0 +1,16 @@
+setwd("C:\\Users\\Udama\\OneDrive\\Desktop\\IT24102178_Lab7")
+
+getwd()
+
+#Q1
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+#Q2
+pexp(2, rate = 1/3) 
+
+#Q3-part1
+1 - pnorm(130, mean = 100, sd = 15)
+
+#Q3-part2
+qnorm(0.95, mean = 100, sd = 15)
+
